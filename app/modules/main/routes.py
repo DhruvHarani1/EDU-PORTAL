72f373ac93068d66c4bc6e62b480a061a0361eab
DhruvHarani1 <dhruvharani5@gmail.com>
2026-02-19 12:43:36 +0530

    feat: Add main module with index page and dedicated feature pages for admin, faculty, and students.

--- app/modules/main/routes.py
+++ app/modules/main/routes.py
@@ -4,3 +4,15 @@ from . import main_bp
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+@main_bp.route('/features/student')
+def features_student():
+    return render_template('features_student.html')
+
+@main_bp.route('/features/faculty')
+def features_faculty():
+    return render_template('features_faculty.html')
+
+@main_bp.route('/features/admin')
+def features_admin():
+    return render_template('features_admin.html')
